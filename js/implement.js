932dcf9e5c46b185dba910e976a8d74480225c01
Yousrahanif <yousrahanif26@gmail.com>
2024-09-22 23:41:21 -0400

    new change

--- js/implement.js
+++ js/implement.js
@@ -3,6 +3,9 @@ function getIdByFunction(id){
 }
 
 function getInputValue(id){
+    return parseFloat(document.getElementById(id).value);
+}
+function getOtherValue(id){
     return parseFloat(document.getElementById(id).innerText);
 }
 function colorSwap(originalBtn, btn, addColor, removeColor){
@@ -27,4 +30,18 @@ colorSwap(historyTab,donationTab, 'bg-blue-300', 'bg-gray-100')
 donationTab.addEventListener('click', function(){
     colorSwap(donationTab,historyTab,'bg-blue-300', 'bg-gray-100')
 })
+const currentValue=getOtherValue('total')
+console.log(currentValue)
+
+const noakhaliHave=getOtherValue('noaHave')
+
+
+// const noakhaliBtn=getIdByFunction('btn-noa');
+// noakhaliBtn.addEventListener('click', function(){
+//     const noaKhaliGet=getInputValue('noaGet')
+//     const newBalance=parseFloat(noakhaliHave+noaKhaliGet).toFixed(2)
+//     document.getElementById('total').innerText=(currentValue+noaKhaliGet).toFixed(2)
+
+// })
+
 
